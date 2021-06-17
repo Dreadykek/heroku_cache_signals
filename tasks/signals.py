36d11dd769c82bac0caf2100f4c9fb2f9cbe338c
Danil <zdanil2000@gmail.com>
2021-06-17 18:29:09 +0500

    New options

--- tasks/signals.py
+++ tasks/signals.py
@@ -6,28 +6,21 @@ from collections import Counter
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
 def task_cats_added(sender, instance, action, model, **kwargs):
-    if action != "post_add":
-        return
+    if action == "post_add":
 
-    for cat in instance.category.all():
-        slug = cat.slug
+        for cat in instance.category.all():
+            slug = cat.slug
 
-        new_count = 0
-        for task in TodoItem.objects.all():
-            new_count += task.category.filter(slug=slug).count()
-
-        Category.objects.filter(slug=slug).update(todos_count=new_count)
+            new_count = Category.objects.filter(slug=slug).first().todos_count
+            Category.objects.filter(slug=slug).update(todos_count= new_count + 1)
 
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
 def task_cats_removed(sender, instance, action, model, **kwargs):
-    if action != "post_remove":
-        return
+    if action == "post_remove":
 
-    cat_counter = Counter()
-    for t in TodoItem.objects.all():
-        for cat in t.category.all():
-            cat_counter[cat.slug] += 1
+        for cat in instance.category.all():
+            slug = cat.slug
 
-    for slug, new_count in cat_counter.items():
-        Category.objects.filter(slug=slug).update(todos_count=new_count)
+            new_count = Category.objects.filter(slug=slug).first().todos_count
+            Category.objects.filter(slug=slug).update(todos_count=new_count - 1)
