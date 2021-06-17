fe9781a5c6e56320609389ccac2b360f0cc54650
Danil <zdanil2000@gmail.com>
2021-06-17 12:22:22 +0500

    V0.3

--- HerokuTest/settings.py
+++ HerokuTest/settings.py
@@ -119,3 +119,30 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+def get_cache():
+    environment_ready = all(
+        os.environ.get(f'MEMCACHIER_{key}', False)
+        for key in ['SERVERS', 'USERNAME', 'PASSWORD']
+    )
+    if not environment_ready:
+        cache = {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
+    else:
+        servers = os.environ['MEMCACHIER_SERVERS']
+        username = os.environ['MEMCACHIER_USERNAME']
+        password = os.environ['MEMCACHIER_PASSWORD']
+        cache = {
+            'default': {
+                'BACKEND': 'django_bmemcached.memcached.BMemcached',
+                'TIMEOUT': None,
+                'LOCATION': servers,
+                'OPTIONS': {
+                    'username': username,
+                    'password': password,
+                }
+            }
+        }
+    return {'default': cache}
+
+
+CACHES = get_cache()
